7081c47456bada308788581166b9f51f46dc4154
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-30 22:07:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['62895336282144', '𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷', true],
-['6282140373458', 'Raxcel-MD ⁴⁰⁴-ﾉ [BOT MD]', true]
+['6287719424487', '𝘼𝙜𝙪𝙣𝙜𝙭 𝙏𝙮𝙪𝙧𝙪»', true],
+['14752659201', '𝙊𝙬𝙣𝙚𝙧 𝘿𝙪𝙖 𝙔𝙣𝙩𝙠𝙩𝙨»', true]
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -34,23 +34,23 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷'
-global.linkgc = 'https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP'
-global.linkig = 'https://www.instagram.com/the.sad.boy01'
+global.WM = 'sᴀᴄʜɪ ʙᴏᴛ'
+global.linkgc = 'https://chat.whatsapp.com/GUZsmmxDNQAAMyzMKrXDTV'
+global.linkig = 'https://www.instagram.com/yt.agungxx'
 global.deslink = ''
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@the.sad.boy01'
+global.bodlink = '@yt.agungxx'
 
-global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
-global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
+global.wait = '_*Sᴇᴅᴀɴɢ Dɪ Pʀᴏsᴇs ...*_'
+global.eror = '_*Sᴇʀᴠᴇʀ Eʀʀᴏʀ..*_'
 
 //========Url Template Buttons==========//
-global.dtu = '𝑭𝒐𝒍𝒍𝒐𝒘'
-global.urlnya = "https://www.instagram.com/the.sad.boy01"
+global.dtu = 'Follow Ig'
+global.urlnya = "https://www.instagram.com/yt.agungxx"
 
 //============= callButtons ============//
-global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
+global.dtc = 'Call Owner'
+global.phn = '+6287719424487'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
